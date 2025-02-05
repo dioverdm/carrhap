ce9bd2fa7d3047eed84b0492bfda584cff28b722
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-02-05 19:37:12 -0400

    Actualizar keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/chat-app",
+  mongoURI: "mongodb+srv://mukuljatav1010:mukuljatav1010@cluster0.qmlj6pq.mongodb.net/?retryWrites=true&w=majority",
   secretOrKey: "ef105311dfce11f3545dc7a8779ef839876d1bc8c2fd15b993efd65ae97d5a4b",
 };
